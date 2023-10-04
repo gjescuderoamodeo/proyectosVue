1ce7f7de8e0ac311679561280e09f7ad557f233a
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-10-04 19:44:24 +0200

    first commit

---
+++ eoi-first-example/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="page">
+        <AppHeader></AppHeader>
+        <div>A webpage from {{ name }}</div>
+        <div v-pre>A webpage from {{ name }}</div> <!--poner literal la variable {{ name }-->
+        <div v-cloak>A webpage from {{ name }}</div> <!--hasta que no cargue no sale {{ name }-->
+        <div v-once>A webpage from {{ name }}</div> <!--solo lo renderiza 1 vez y da igual lo modifiques-->
+
+        <input type="text" v-model="name"> <!--enlazar modelo como en angular-->
+    </div>
+
+    <div class="page2">
+        <ol>
+            <li v-for="user, index in users" :key="index"> {{ user.name }} {{ index }}</li>
+        </ol>
+        <!--Bucle números
+        <ol>
+            <li v-for="r in rango" :key="index">{{ r }}</li>
+        </ol>-->
+
+        <button @click="greets">pulseme</button>
+    </div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+    name: "App",
+    components: {
+        AppHeader
+    },
+    data() {
+        //almacen variables (reactivas) de vue
+        return {
+            name: "pepe",
+            role: "test",
+            users: [
+                { name: "pepe" },
+                { name: "pepe" },
+                { name: "pepe" }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+body {
+    background-color: black;
+}
+
+.page {
+    background: blueviolet;
+    width: 300px;
+    height: 300px;
+}
+
+.page2 {
+    background: rgb(67, 125, 1);
+    width: 300px;
+    height: 300px;
+}
+</style>
---
+++ eoi-first-example/src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <h1>test</h1>
+        <img :src="imageUrl" :alt="imageText"> <!-- para que coja la variable -->
+    </header>
+</template>
+
+<script>
+export default {
+    name: "AppHeader", //nombre para el componente
+    data() {
+        return {
+            imageText: "michi",
+            imageUrl: "https://www.record.com.mx/sites/default/files/styles/v2-crop768x433/public/articulos/2023/08/08/cat-3266673_1280.jpg?itok=818wX96J"
+        }
+    }
+}
+</script>
+
+<style>
+img {
+    width: 200px;
+    height: auto;
+
+}
+</style>
