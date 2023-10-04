65b6bfd0019aaec5e82c3a37277208fde81c6225
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-10-04 20:24:45 +0200

    Update AppHeader.vue

--- eoi-first-example/src/components/AppHeader.vue
+++ eoi-first-example/src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <header>
-        <h1>test</h1>
+        <h1>{{ info }}</h1>
         <img :src="imageUrl" :alt="imageText"> <!-- para que coja la variable -->
     </header>
 </template>
@@ -13,6 +13,15 @@ export default {
             imageText: "michi",
             imageUrl: "https://www.record.com.mx/sites/default/files/styles/v2-crop768x433/public/articulos/2023/08/08/cat-3266673_1280.jpg?itok=818wX96J"
         }
+    },
+    //atributos del componente ; son constantes
+    props: {
+        "info": {
+            type: String,
+            default: "test",
+            validator: (value) => { return (value[0] === "P") }
+        }
+
     }
 }
 </script>
