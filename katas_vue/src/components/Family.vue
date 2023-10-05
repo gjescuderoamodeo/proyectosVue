9510eefbc7142bfc8139636fc9a18e9d4cdb76d2
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-10-05 18:42:29 +0200

    Update Family.vue

--- katas_vue/src/components/Family.vue
+++ katas_vue/src/components/Family.vue
@@ -1,12 +1,56 @@
 <template>
   <div>
-
+    <input placeholder="Introduce la edad">
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      totalCount: 0,
+      family: [{
+        name: 'pepe',
+        age: 57,
+        child: [{
+          name: 'susana',
+          age: 12,
+          child: []
+        },
+        {
+          name: 'julia',
+          age: 20,
+          child: [
+            {
+              name: 'jose',
+              age: 1,
+              child: []
+            },
+          ]
+        }]
+      }]
+    }
+  },
+  beforeMount() {
+    this.getCountPersonsWithAgeHighest(12);
+  },
+  methods: {
+    calculateTotalCount(family, age) {
+      family.forEach(member => {
+        /*if (member.age > age) {
+          this.totalCount++;
+        }*/
+        console.log(member.name);
+        if (member.child.length === 0) {
+          return;
+        }
+        this.calculateTotalCount(member.child, age);
+      });
+    },
+    getCountPersonsWithAgeHighest(age) {
+      this.calculateTotalCount(this.family, 12);
+    }
+  }
 }
 </script>
 
