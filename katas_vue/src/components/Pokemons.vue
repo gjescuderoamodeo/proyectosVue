417fa15e4cc74f9cc0442c0722a2c89f17b179bf
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-10-05 20:08:07 +0200

    Update Pokemons.vue

--- katas_vue/src/components/Pokemons.vue
+++ katas_vue/src/components/Pokemons.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    
+    <button type="button" @click="setheightByPokemonId(25)">heightByPokemonId</button>
+    Altura es {{ heightByPokemonId }}
   </div>
 </template>
 
@@ -8,11 +9,21 @@
 export default {
   data() {
     return {
-
+      heightByPokemonId: 0,
+      pokemonsByType: '',
+      pokemonSpeciesName: '',
+      heightsByType: '',
     }
   },
   methods: {
-    
+    async setheightByPokemonId(pokemonId) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+      await fetch(url)
+        .then(response => response.json())
+        .then(pokemon => {
+          this.heightByPokemonId = pokemon.height;
+        });
+    }
   },
 
 }
