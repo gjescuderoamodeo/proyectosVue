33dd72ac2d72fde09165388011daef4909699a77
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-10-11 18:11:30 +0200

    Create DigitalClock

---
+++ DigitalClock/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ActualHour from './components/ActualHour.vue'
+</script>
+
+<template>
+  <div class="clock">
+    <ActualHour />
+  </div>
+</template>
+
+<style scoped>
+.clock {
+  border: 0.3rem solid white;
+  width: 30rem;
+  height: 10rem;
+}
+</style>
---
+++ DigitalClock/src/components/ActualHour.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    {{ horaActual }} : {{ minutosActual }} : {{ segundosActual }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ActualHour",
+  data() {
+    return {
+      horaActual: '',
+      minutosActual: '',
+      segundosActual: ''
+    };
+  },
+  created() {
+    // Actualizar la hora cada segundo
+    setInterval(() => {
+      this.obtenerHoraActual();
+    }, 1000);
+  },
+  methods: {
+    obtenerHoraActual() {
+      const ahora = new Date();
+      const hora = ahora.getHours().toString().padStart(2, '0');
+      const minutos = ahora.getMinutes().toString().padStart(2, '0');
+      const segundos = ahora.getSeconds().toString().padStart(2, '0');
+      this.horaActual = `${hora}`;
+      this.minutosActual = `${minutos}`;
+      this.segundosActual = `${segundos}`;
+    }
+  }
+}
+</script>
+
+<style>
+/* Estilos CSS aquí si es necesario */
+</style>
