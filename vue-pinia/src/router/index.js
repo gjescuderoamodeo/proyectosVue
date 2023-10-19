96a9bfaeaffa067ed3cedbd680ea6c9f65796c3f
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-10-19 17:41:45 +0200

    Update index.js

--- vue-pinia/src/router/index.js
+++ vue-pinia/src/router/index.js
@@ -5,11 +5,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
       path: '/vista1',
       name: 'vista1',
       component: HomeView
