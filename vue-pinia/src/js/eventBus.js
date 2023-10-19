4aec7a198cb2166c537157bc9a77ad1a3d713392
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-10-19 18:31:38 +0200

    eventbus

---
+++ vue-pinia/src/js/eventBus.js
@@ -0,0 +1,10 @@
+// eventBus.js
+//npm install tiny-emitter --save
+import emitter from 'tiny-emitter/instance'
+
+export default {
+    $on: (...args) => emitter.on(...args),
+    $once: (...args) => emitter.once(...args),
+    $off: (...args) => emitter.off(...args),
+    $emit: (...args) => emitter.emit(...args)
+}
